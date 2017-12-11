d6f0c2ed399c9a85c0c87fc71aa5cfcb176dff5f
Cherie Lum <charismatic.cherie@gmail.com>
2017-12-10 19:57:04 -0500

    adding paddle parameters

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var canvas; 
 var vancasContext; 
 var ballX = 50;  
-var ballSpeedX = 15; 
+var ballY = 50;
+var ballSpeedX = 10; 
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HEIGHT =100;
+
+//function for mouse
+function calculateMousePos(event) {
+    var rect = canvas.getBoundingClientRect(); 
+    var root = document.documentElement; 
+    var mouseX = event.clientX -rect.left - root.scrollLeft; 
+    var mouseY = event.clientY -rect.top - root.scrollTop;
+    return {
+        x:mouseX, 
+        y:mouseY
+    } 
+}
+
 
 window.onload =function() {
     console.log("hello world!");
@@ -16,28 +34,65 @@ window.onload =function() {
             moveEverything(); 
             drawEverything(); 
         }, 1000/framesPerSecond);
+
+    canvas.addEventListener('mousemove',
+        function(event) {
+            var mousePos = calculateMousePos(event); 
+            paddle1Y = mousePos.y-(PADDLE_HEIGHT/2); 
+        });
+}
+
+//reset ball means it centers it on the screen
+function ballReset(){
+      //put ball in opposite direction
+      ballSpeedX = -ballSpeedX;
+    
+    ballX = canvas.width/2; 
+    ballY = canvas.height/2;
+
+  
 }
 
 
 function moveEverything(){
     ballX = ballX + ballSpeedX;
-
-    if(ballX >= canvas.width) {
-        ballSpeedX = -ballSpeedX;
-    }
+    ballY = ballY + ballSpeedY;
     if(ballX < 0) {
+        //ballSpeedX = -ballSpeedX;
+        if(ballY > paddle1Y && 
+           ballY < paddle1Y + PADDLE_HEIGHT) {
+               ballSpeedX = -ballSpeedX; 
+           } else {
+                ballReset();
+           }
+        
+    }
+    if(ballX > canvas.width) {
         ballSpeedX = -ballSpeedX;
     }
-   
+    if(ballY < 0) {
+        ballSpeedY = -ballSpeedY;
+    }
+    if(ballY >= canvas.height) {
+        ballSpeedY = -ballSpeedY;
+    }
 }
 
 function drawEverything(){
     //next line blanks out the screen with black
     colorRect(0,0,canvas.width, canvas.height, 'black');
     //this is left player paddle
-    colorRect(0, 210, 10,100, 'white');
-    //this is the red ball
-    colorRect(ballX, 100, 10, 10, 'red');
+    colorRect(0, paddle1Y, 10,PADDLE_HEIGHT, 'white');
+    //this is the ball
+    // colorRect(ballX, 100, 10, 10, 'red');
+    colorCircle(ballX, ballY, 10, 'white');
+}
+
+function colorCircle(centerX, centerY, radius, drawColor ){
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath(); 
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+    canvasContext.fill(); 
 }
 
 function colorRect(leftX, topY, width, height, drawColor){
